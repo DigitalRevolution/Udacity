bf6e522fc9b735685139da8d223f5b70aa46330f
Digital Revolution <stephen.vandelinder@gmail.com>
2016-01-25 23:39:33 -0700

    Admin section form and functionality added

--- CatClickerProPremium/js/app.js
+++ CatClickerProPremium/js/app.js
@@ -1,40 +1,50 @@
-$(function(){
+//$(function(){
 
 // Model 
 	var model = {
+		currentCatNum: null, 
 		cats : [
 				{
 					'name' : 'Amber',
-					'img' : 'cat1.jpg',
+					'img' : './img/cat1.jpg',
 					'clicks' : 0
 				},
 				{
 					'name' : 'Blue',
-					'img' : 'cat2.jpg',
+					'img' : './img/cat2.jpg',
 					'clicks' : 0
 				},
 				{
 					'name' : 'Cyan',
-					'img' : 'cat3.jpg',
+					'img' : './img/cat3.jpg',
 					'clicks' : 0
 				},
 				{
 					'name' : 'Dusty',
-					'img' : 'cat4.jpg',
+					'img' : './img/cat4.jpg',
 					'clicks' : 0
 				},
 				{
 					'name' : 'Fucia',
-					'img' : 'cat5.jpg', 
+					'img' : './img/cat5.jpg', 
 					'clicks' : 0
 				}
-			]
+			], 
+		update : function(catNum) {
+			this.cats[catNum].name = $('form').find('input[name = "catname"]').val();
+			this.cats[catNum].img = $('form').find('input[name = "catimg"]').val();
+			this.cats[catNum].clicks = $('form').find('input[name = "catclicks"]').val();
+			view.adminSection.clearAndClose();  
+			view.catSection.render(catNum);
+			view.catMenu.render();
+		}
 	}; 
 
 	// Controller 
 	var controller = {
 
 		init: function(){
+			model.currentCatNum = 0; 
 			view.catMenu.init();
 			view.catSection.init(); 
 		}, 
@@ -48,17 +58,26 @@ $(function(){
 	// click handlers -- use delegation so $(this) doesn't bind to something crazy. 
 
 	$('.cat-section').on('click', '.cat-photo', function() {
-		var catNum = $(this).data('num'); 
-		controller.increment(catNum);
+		model.currentCatNum = $(this).data('num'); 
+		controller.increment(model.currentCatNum);
 	});
 
 	$('.cat-menu').on('click', '.cat-selector', function(e) {
 		e.preventDefault();
-		console.log($(this).attr('href')); 
-		var catNum = $(this).attr('href');
-		controller.increment(catNum); 
+//		console.log($(this).attr('href')); 
+		model.currentCatNum = $(this).attr('href');
+		controller.increment(model.currentCatNum); 
 	});
 
+	$('.admin-button').click( function() {
+		$('.admin-section').show(); 
+	}); 
+	$('.admin-cancel').click( function() {
+		view.adminSection.clearAndClose();  
+	})
+	$('.admin-save').click( function() {
+		model.update(model.currentCatNum); 
+	})
 
 	// View
 	var view = {
@@ -66,13 +85,11 @@ $(function(){
 		catMenu : {
 
 			init : function(){
-
 				this.render(); 
-
 			},
 
 			render: function(){
-
+				$('.cat-menu').html(''); 
 				for (var i = 0; i < model.cats.length; i++){
 					$('.cat-menu').append(
 						'<li>' +
@@ -85,22 +102,28 @@ $(function(){
 		catSection : {
 
 			init : function(){
-
 				this.render(0); 
-		
 			},
 
 			render: function(catNum){
 				$('.cat-section').html(
 				'<h3>' + model.cats[catNum].name + 
 				'</h3><img class = "cat-photo" data-num = "' + catNum + 
-				'" src = "./img/' + model.cats[catNum].img + 
+				'" src = "' + model.cats[catNum].img +  
 				'"><h3>' + model.cats[catNum].clicks + '</h3>'
 				); 
+			} 
+		}, 
+
+		adminSection : {
+
+			clearAndClose : function() {
+				$('form').find('input[type = text]').val('');
+				$('.admin-section').hide();
 			}
 		}
 	}; 
 
 	controller.init(); 
 
-}); 
+//}); 
